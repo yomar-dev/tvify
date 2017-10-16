c351f5128bf02708503bc0d910da142b42c4b58f
Yomar Miranda <mirandayomar@gmail.com>
2017-10-16 15:44:16 -0500

    Creamos una variable que nos va a servir de template para mostrar cada una de las series

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ $(function(){
 			var busqueda = $(this).find('input[type="text"]').val();
 		})
 
+	var template = '<article class="tv-show">' +
+					'<div class="left">' +
+						'<img src=":img:" alt=":img alt:">' +
+					'</div>'+
+
+					'<div class="left info">' +
+						'<h1>:name:</h1>' +
+						'<p>:summary:</p>' +
+					'</div>'+
+				'</article>';
+
 	$.ajax({
 		url: 'http://api.tvmaze.com/shows',
 		success: function(data, textStatus, xhr){

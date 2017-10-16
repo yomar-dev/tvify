6750339ee611c8e3455c82de5fa84835ef25dcc5
Yomar Miranda <mirandayomar@gmail.com>
2017-10-16 15:27:11 -0500

    Realizamos la petición a la API de TVMaze y nos retorna una lista de series

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ $(function(){
 			e.preventDefault();
 			var busqueda = $(this).find('input[type="text"]').val();
 		})
+
+	$.ajax({
+		url: 'http://api.tvmaze.com/shows',
+		success: function(data, textStatus, xhr){
+			console.log(data);
+		}
+	})
 })
